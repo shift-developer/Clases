093b8f7a266524a15771357d734867e05e334129
Juan Shift <61192769+shift-developer@users.noreply.github.com>
2020-06-12 21:13:00 -0300

    Update Ejercicio-4.js

---
+++ Clase 29 Promesas/Ejercicio-4.js
@@ -0,0 +1,20 @@
+let promesaRace1 = new Promise(
+    (resolve, reject) => {
+        let random = Math.random()*1000;
+        setTimeout( () => {
+            resolve('Ganó la carrera la promesaRace1. Tiempo: ' + random + 'ms');
+        }, random);
+    }
+);
+
+let promesaRace2 = new Promise(
+    (resolve, reject) => {
+        let random = Math.random()*1000;
+        setTimeout( () => {
+            resolve('Ganó la carrera la promesaRace2. Tiempo: ' + random + 'ms');
+        }, random);
+    }
+);
+
+Promise.race([promesaRace1, promesaRace2])
+    .then(res => console.log(res));

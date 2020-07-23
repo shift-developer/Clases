07656128b354c8ad4cc8c7bc7a35774f225089e4
Juan Shift <61192769+shift-developer@users.noreply.github.com>
2020-07-23 18:46:27 -0300

    server1

---
+++ clase43-Express/server-1.js
@@ -0,0 +1,66 @@
+const app = express();
+
+app.listen( 3000, () => {
+    console.log('Servidor Iniciado');
+});
+
+//express().verboHTTP(ruta, callback( request, response))
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo!');
+});
+
+app.get('/error', (req, res) => {
+    res.status(500);
+    res.json({ error: 'Hubo un error' });
+});
+
+//https://jsonplaceholder.typicode.com/photos/1?size=600
+
+app.get('/photos', (req, res) => {
+    res.json([
+    {
+        id:1,
+        title: 'asdasda',
+        url: 'https://via.placeholder.com/600/92c952'
+    }, 
+    {
+        id: 2,
+        title: 'asdasd2',
+        url: 'https://via.placeholder.com/600/771796'
+    }
+    ]);
+});
+
+const fotos = [
+    {
+        id:1,
+        title: 'asdasda',
+        url: 'https://via.placeholder.com/600/92c952'
+    }, 
+    {
+        id: 2,
+        title: 'asdasd2',
+        url: 'https://via.placeholder.com/600/771796'
+    }
+    ];
+    
+//Definicion de la ruta de express que retorna una foto especifica
+
+/*app.get('/photos/:indiceFoto', (req, res) => {
+    const indiceFoto = req.params.indiceFoto;
+    res.json(fotos[indiceFoto]);
+});
+*/
+
+app.get('photos/:indicefoto', (req, res) => {
+    //obtenemos parametro de la foto
+    const { indiceFoto } = req.params;
+
+    //req.query contiene todos los query strings enviados
+    const { size } = req.query;
+
+    const foto = fotos[indiceFoto];
+    foto.url.replace('/600', `/${size}`);
+        res.json(foto);
+});
